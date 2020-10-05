f811e0913d3a9985cd8859d3a9457d6273782479
coyo-tk <70998177+coyo-tk@users.noreply.github.com>
2020-10-05 09:59:15 +0300

    Add files via upload

---
+++ da.asm
@@ -0,0 +1,17 @@
+mov F881, r0
+out r0, p0
+mov F008, r0
+out r0, p1
+mov E400, r0
+out r0, p2
+mov C091, r0
+out r0, p3
+mov 8000, r0
+out r0, p4
+mov 824, r0
+out r0, p5
+mov 0000, r0
+out r0, p6
+mov 2111, r0
+out r0, p7
+
